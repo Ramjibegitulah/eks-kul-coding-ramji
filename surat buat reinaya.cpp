8294c26c3af8516a6035410d60260f60246baca9
Ramjibegitulah <113175977+Ramjibegitulah@users.noreply.github.com>
2022-09-23 17:13:04 +0700

    Initial commit
    ini adalah surat dari orang yang bernama zafran untuk reinaya. BTW, ini juga sebagai tugas buat ekskul saya

---
+++ surat buat reinaya.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+	// \t digunakan untuk spasi lebIh panjang
+	// sedagkan\n digunakan untuk enter
+	cout<<"Dear, \n";
+	cout<<"My little Reinaya \n";
+	cout<<"Dimanapun Kamu berada \n\n";
+	
+	cout<<"Beberapa hari belakang ini, aku merasa kesepian ... \n";
+	cout<<"Aku bingung, bagaimana aku bisa menjadi lebih semangat jika tidak dirimu di sekolah ini .... \n";
+	cout<<"Aku yang biasanya senang dan gembira, sekarang aku menjadi pendiam  .... \n";
+	cout<<"Tapi aku tau kau pasti sedang sibuk disana .....\n";
+	cout<<"Tapi entah mengapa aku sangat kesepian tanpa dirimu ....\n";
+	
+	cout<<"Jadi aku hanya ingin bilang ....\n";
+	cout<<"Jangan lupa sholat 5 waktu ....\n\n";
+	
+	cout<<"\t\t\t\t Palembang, 2 September 2022\n";
+	cout<<"\t\t\t\t Your's Best Friend,\n";
+	cout<<"\t\t\t\t Zafram .....";
+	
+	getch();
+	
+	return 0;
+}
